a9e04959c262dd5cd848b5689fa1f2f72c205da7
Amy Frieson <ava447@gmail.com>
2018-10-19 09:54:18 -0400

    Improve instructions, handle 0-indexing, update README

--- javascript/README.md
+++ javascript/README.md
@@ -1,11 +1,13 @@
 # Tic Tac Toe for Command Line Games
 
-Fork or clone the repository to your machine. This version of the project is in JavaScript, so `cd` into the `javascript` directory.
-
 ## To play the game
 
-Run `node game.js` in the terminal and follow the directions!
+Fork or clone the repository to your machine. This version of the project is in JavaScript, so `cd` into the `javascript` directory.
 
-## To further develop or test
+This game runs in Node and was developed with version `10.9.0`. If not installed, Node can be downloaded directly [from their website](https://nodejs.org/en/).
 
-This game uses Jasmine as a testing framework. Install dependencies with `npm install`. Run the tests with `npm test`.
+After downloading, run `node game.js` in the terminal and follow the directions!
+
+## Further testing and development
+
+Tic Tac Toe uses Jasmine as a testing framework. To test, or to edit the tests, first install dependencies with `npm install`, and then run the tests with `npm test`.
--- javascript/game.js
+++ javascript/game.js
@@ -86,11 +86,11 @@ const model = {
     // TODO name formatted like Player 1?
     this.players = [
       {
-        name: 'player1',
+        name: 'Player 1',
         data: 1
       },
       {
-        name: 'player2',
+        name: 'Player 2',
         data: 2
       }
     ];
@@ -98,15 +98,15 @@ const model = {
     this.gameWon = false;
   },
 
+  setPlayerData: function (player, key, value) {
+    const position = player - 1;
+    this.players[position][key] = value;
+  },
+
   setStartingPlayer: function (player, selection) {
     if (this.currentPlayer === null && selection === 'y') {
       this.currentPlayer = player;
     }
-  },
-
-  setPlayerData: function (player, key, value) {
-    const position = player - 1;
-    this.players[position][key] = value;
   },
 
   shareBoardData: function () {
@@ -142,8 +142,40 @@ const controller = {
       .then(() => this.runPlaySequence());
   },
 
-  show: function () {
-    return this.view.renderBoard(this.model.shareBoardData(), this.model.sharePlayerSymbols());
+  checkForGameOver: function () {
+    if (this.checkForWin() || this.checkIfBoardFilled()) {
+      this.exit();
+    }
+  },
+
+  checkForWin: function () {
+    const patterns = this.model.sharePatterns();
+    const checkWinningPatterns = this.checkPatterns(patterns[2]);
+    if (checkWinningPatterns === -1 && this.model.gameWon === false) {
+      return false;
+    } else {
+      this.model.gameWon = true;
+      this.view.sayMessage(this.view.messages.gameWon, this.model.currentPlayer);
+      return true;
+    }
+  },
+
+  checkIfBoardFilled: function () {
+    if (this.model.board.join('').indexOf(0) === -1) {
+      this.view.sayMessage(this.view.messages.gameOver);
+      return true;
+    }
+    return false;
+  },
+
+  checkPatterns: function (patternsToCheck) {
+    const boardString = this.model.board.join('');
+    for (let i = 0; i < patternsToCheck.length; i++) {
+      if (boardString.match(patternsToCheck[i][0])) {
+        return patternsToCheck[i][1];
+      }
+    }
+    return -1;
   },
 
   computerPickSpace: function (currentPlayer) {
@@ -172,40 +204,15 @@ const controller = {
       .then(() => this.updatePlayer(1, 'position'))
       .then(() => this.updatePlayer(2, 'type'))
       .then(() => this.updatePlayer(2, 'symbol'))
-      .then(() => this.updatePlayer(2, 'position'));
+      .then(() => this.updatePlayer(2, 'position'))
+      .then(() => this.view.getPlayerSymbols(this.model.sharePlayerSymbols()));
   },
 
-  // TODO separate concerns in below function
-  // TODO handle bad data entry
-  // TODO implement catch
-  updatePlayer: function (player, prop) {
-    const boundModel = this.model;
-    const boundView = this.view;
-    // return early if currentPlayer has already been assigned
-    if (prop === 'position' && this.model.shareCurrentPlayer()) {
-      return;
-    }
-    boundView.sayMessage(boundView.messages.playerSetup[prop]);
-    const playerData = new Promise(function (resolve, reject) {
-      boundView.handleUserInput()
-        .then(selection => {
-          if (prop === 'position') {
-            boundModel.setStartingPlayer(player, selection);
-          } else {
-            if (prop === 'type' && selection === '1') {
-              selection = 'human';
-            } else if (prop === 'type' && selection === '2') {
-              selection = 'computer';
-            }
-            boundModel.setPlayerData(player, prop, selection);
-          }
-          resolve(player[prop]);
-        // reject(console.log('Sorry, this data was invalid. Please make another selection.'));
-        });
-    });
-    return playerData;
+  exit: function () {
+    process.exit();
   },
 
+  // TODO correct for zero-indexed array based on directions
   // executes after player gives valid input or computer fn has selected a space
   move: function (chosenSpace, playerData) {
     // checks that the correct player is moving
@@ -222,75 +229,18 @@ const controller = {
     return false;
   },
 
-  checkIfBoardFilled: function () {
-    if (this.model.board.join('').indexOf(0) === -1) {
-      this.show();
-      this.view.sayMessage(this.view.messages.gameOver);
-      return true;
-    }
-    return false;
-  },
-
-  checkForGameOver: function () {
-    if (this.checkForWin() || this.checkIfBoardFilled()) {
-      this.exit();
-    }
-  },
-
-  checkPatterns: function (patternsToCheck) {
-    const boardString = this.model.board.join('');
-    for (let i = 0; i < patternsToCheck.length; i++) {
-      if (boardString.match(patternsToCheck[i][0])) {
-        return patternsToCheck[i][1];
-      }
-    }
-    return -1;
-  },
-
-  checkForWin: function () {
-    const patterns = this.model.sharePatterns();
-    const checkWinningPatterns = this.checkPatterns(patterns[2]);
-    if (checkWinningPatterns === -1 && this.model.gameWon === false) {
-      return false;
-    } else {
-      this.model.gameWon = true;
-      this.show();
-      this.view.sayMessage(this.view.messages.gameWon);
-      return true;
-    }
-  },
-
-  exit: function () {
-    process.exit();
-  },
-
-  runPlaySequence: function () {
-    this.show();
-    this.view.sayMessage(this.view.messages.instructions);
-    // only nine moves are possible in a game of tic tac toe
-    return this.play()
-      .then(() => this.play())
-      .then(() => this.play())
-      .then(() => this.play())
-      .then(() => this.play())
-      .then(() => this.play())
-      .then(() => this.play())
-      .then(() => this.play())
-      .then(() => this.play())
-      .then(() => this.checkPatterns(this.model.sharePatterns()[3]))
-      .catch((e) => console.log(e));
-  },
-
   play: function () {
     const boundController = this;
     const boundModel = this.model;
     const boundView = this.view;
     const currentPlayer = this.model.shareCurrentPlayer();
     const play = new Promise(function (resolve, reject) {
+      boundController.show();
+      boundView.sayMessage(boundView.messages.turn, currentPlayer.data);
       if (currentPlayer.type === 'human') {
         boundView.handleUserInput()
           .then(space => {
-            if (boundController.move(space, currentPlayer.data)) {
+            if (boundController.move(space - 1, currentPlayer.data)) {
               boundController.show();
               boundController.checkForGameOver();
               boundModel.toggleCurrentPlayer();
@@ -313,6 +263,58 @@ const controller = {
       }
     });
     return play;
+  },
+
+  // only nine moves are possible in a game of tic tac toe
+  runPlaySequence: function () {
+    return this.view.renderInstructions()
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.play())
+      .then(() => this.checkPatterns(this.model.sharePatterns()[3]))
+      .catch((e) => console.log(e));
+  },
+
+  show: function () {
+    const board = this.view.convertBoardString(this.model.shareBoardData());
+    this.view.renderBoard(board);
+  },
+
+  // TODO separate concerns in below function
+  // TODO handle bad data entry
+  // TODO implement catch
+  updatePlayer: function (player, prop) {
+    const boundModel = this.model;
+    const boundView = this.view;
+    // return early if currentPlayer has already been assigned
+    if (prop === 'position' && this.model.shareCurrentPlayer()) {
+      return;
+    }
+    boundView.sayMessage(boundView.messages.playerSetup[prop], player);
+    const playerData = new Promise(function (resolve, reject) {
+      boundView.handleUserInput()
+        .then(selection => {
+          if (prop === 'position') {
+            boundModel.setStartingPlayer(player, selection);
+          } else {
+            if (prop === 'type' && selection === '1') {
+              selection = 'human';
+            } else if (prop === 'type' && selection === '2') {
+              selection = 'computer';
+            }
+            boundModel.setPlayerData(player, prop, selection);
+          }
+          resolve(player[prop]);
+          // reject(console.log('Sorry, this data was invalid. Please make another selection.'));
+        });
+    });
+    return playerData;
   }
 };
 
@@ -321,17 +323,44 @@ const view = {
   init: function () {
     this.messages = {
       gameOver: 'Game over',
-      gameWon: 'Game won',
-      instructions: 'Enter [0-8]:',
+      gameWon: 'Player num wins!',
+      instructions: 'Instructions: \nEnter [1-9] to make your move. \nThe spaces in the board above are currently marked to show which key corresponds with which space. \nPress ENTER to play!',
       invalidEntry: 'That was not a valid move',
+      player1: 'Player 1',
+      player2: 'Player 2',
       playerSetup: {
-        position: 'Will Player 1 play first? Enter y/n.',
-        symbol: 'What symbol will Player 1 use? Enter the symbol and press enter to confirm.',
-        type: 'Is Player 1 a human or computer? Enter \'1\' for human and \'2\' for computer.'
+        position: 'Will Player num play first? Enter y/n.',
+        symbol: 'What symbol will Player num use? Enter the symbol and press enter to confirm.',
+        type: 'Is Player num a human or computer? Enter \'1\' for human and \'2\' for computer.'
       },
-      welcome: 'Welcome to Tic Tac Toe! First you\'ll need to create your players. We\'ll set up one player at a time.'
+      turn: 'It\'s Player num\'s turn...',
+      welcome: 'Welcome to Tic Tac Toe! \nFirst you\'ll need to create your players. We\'ll set up one player at a time.'
     };
+    this.board = `
+     symbol | symbol | symbol
+    ===+===+===
+     symbol | symbol | symbol
+    ===+===+===
+     symbol | symbol | symbol
+
+    `;
     this.sayMessage(this.messages.welcome);
+  },
+
+  convertBoardString: function (boardData) {
+    return boardData.map(space => {
+      if (space === 0) {
+        return ' ';
+      } else if (space === 1) {
+        return this.player1Symbol;
+      } else if (space === 2) {
+        return this.player2Symbol;
+      }
+    });
+  },
+
+  getPlayerSymbols: function (playerSymbols) {
+    [ this.player1Symbol, this.player2Symbol ] = playerSymbols;
   },
 
   handleUserInput: function () {
@@ -344,25 +373,29 @@ const view = {
     return input;
   },
 
-  // TODO "the existing code is so coupled to the console"
-  renderBoard: function (boardData, playerSymbols) {
-    const [ player1Symbol, player2Symbol ] = playerSymbols;
-    const boardSymbols = boardData.map(space => {
-      if (space === 0) {
-        return ' ';
-      } else if (space === 1) {
-        return player1Symbol;
-      } else if (space === 2) {
-        return player2Symbol;
-      }
+  renderBoard: function (boardString) {
+    console.clear();
+    let newBoard = this.board;
+    boardString.forEach(symbol => {
+      newBoard = newBoard.replace('symbol', symbol);
     });
-    console.log(' ' + boardSymbols[0] + ' |' + ' ' + boardSymbols[1] + ' |' + ' ' + boardSymbols[2] + '\n===+===+===\n' +
-    ' ' + boardSymbols[3] + ' |' + ' ' + boardSymbols[4] + ' |' + ' ' + boardSymbols[5] + '\n===+===+===\n' +
-    ' ' + boardSymbols[6] + ' |' + ' ' + boardSymbols[7] + ' |' + ' ' + boardSymbols[8]);
+    console.log(newBoard);
   },
 
-  sayMessage: function (message) {
-    console.log(message);
+  renderInstructions: function () {
+    const boundView = this;
+    const instructions = new Promise(function (resolve, reject) {
+      boundView.renderBoard([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      boundView.sayMessage(boundView.messages.instructions);
+      process.openStdin().once('data', function () {
+        resolve(console.log('done'));
+      });
+    });
+    return instructions;
+  },
+
+  sayMessage: function (message, player) {
+    console.log(message.replace('num', player));
   }
 };
 
